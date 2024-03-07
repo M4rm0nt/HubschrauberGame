d8873c6d2d5ee7d2643db14eb7cd61ed9aa9fb62
marmont <mar.mont@example.com>
2024-03-07 18:18:22 +0100

    Update

--- game.py
+++ game.py
@@ -1,35 +1,37 @@
 import pygame
 import random
 import os
+import sys
 
 # Initialisierung von Pygame
 pygame.init()
 
 # Bildschirmeinstellungen
-screen_width, screen_height = 600, 400
+screen_width, screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("ErzCollector")  # Setzen des Fenstertitels
 
 # Farbdefinitionen
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 # Spieluhr und FPS
 clock = pygame.time.Clock()
 FPS = 60
-
 
 # Lade Bilder
 def load_image(name):
     path = os.path.expanduser(f'~/Dokumente/pics/{name}.png')
     return pygame.image.load(path)
 
-
 # Sprite-Klassen
 class LKW(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = load_image('lastwagen')  # Bild für den LKW
+        self.image = load_image('lastwagen')
         self.rect = self.image.get_rect(center=(screen_width // 2, screen_height - 50))
+        self.hitbox = pygame.Rect(self.rect.left + 10, self.rect.top + 10, self.rect.width - 20, self.rect.height - 20)  # Anpassung der Hitbox
         self.geschwindigkeit = 5
         self.sprit = 100
         self.erz = 0
@@ -50,6 +52,7 @@ class LKW(pygame.sprite.Sprite):
             self.rect.y = max(0, self.rect.y - self.geschwindigkeit)
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.rect.y = min(screen_height - self.rect.height, self.rect.y + self.geschwindigkeit)
+        self.hitbox.center = self.rect.center
 
     def consume_fuel(self):
         if any(pygame.key.get_pressed()):
@@ -58,60 +61,116 @@ class LKW(pygame.sprite.Sprite):
                 self.kill()
 
     def check_collisions(self, erzquelle, ablageplatz, tankstelle, hubschrauber_group):
-        if pygame.sprite.spritecollide(self, hubschrauber_group, dokill=False):
+        collided_hubschrauber = pygame.sprite.spritecollideany(self, hubschrauber_group, collided=lambda s1, s2: s1.hitbox.colliderect(s2.rect))
+        if collided_hubschrauber:
             self.erz = 0
-        if pygame.sprite.collide_rect(self, erzquelle):
+            collided_hubschrauber.zurücksetzen_erforderlich = True
+        if self.hitbox.colliderect(erzquelle.rect):
             self.collect_erz(erzquelle)
-        if pygame.sprite.collide_rect(self, ablageplatz):
+        if self.hitbox.colliderect(ablageplatz.rect):
             ablageplatz.erz += self.erz
             self.erz = 0
-        if pygame.sprite.collide_rect(self, tankstelle):
+        if self.hitbox.colliderect(tankstelle.rect):
             self.sprit = 100
 
     def collect_erz(self, erzquelle):
-        if erzquelle.erz_menge > 0:
-            abzubauendes_erz = min(50, erzquelle.erz_menge, self.max_erz - self.erz)
-            self.erz += abzubauendes_erz
-            erzquelle.erz_menge -= abzubauendes_erz
-            if self.erz >= self.max_erz or erzquelle.erz_menge == 0:
-                erzquelle.neu_positionieren()
+        if self.erz < self.max_erz:
+            if erzquelle.erz_menge > 0:
+                abzubauendes_erz = min(self.max_erz - self.erz, erzquelle.erz_menge)
+                self.erz += abzubauendes_erz
+                erzquelle.erz_menge -= abzubauendes_erz
+                if self.erz == abzubauendes_erz:
+                    erzquelle.neu_positionieren()
 
+class Helipad(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = load_image('helipad')
+        self.rect = self.image.get_rect(center=(random.randint(50, screen_width - 150), random.randint(50, screen_height - 50)))
 
 class Hubschrauber(pygame.sprite.Sprite):
-    def __init__(self, lkw):
+    def __init__(self, lkw, helipad):
         super().__init__()
-        self.image = load_image('helicopter')  # Bild für den Hubschrauber
-        self.rect = self.image.get_rect(center=(random.choice([0, screen_width]), random.randint(0, screen_height)))
-        self.geschwindigkeit = 3
+        self.image = load_image('helicopter')
+        self.rect = self.image.get_rect(center=helipad.rect.center)
+        self.geschwindigkeit = 2.5
         self.lkw = lkw
+        self.helipad = helipad
+        self.zurücksetzen_erforderlich = False
 
-    def update(self, *args, **kwargs):
-        if self.lkw.rect.x < self.rect.x:
+    def update(self):
+        if self.zurücksetzen_erforderlich:
+            self.reset_to_helipad()
+        else:
+            self.follow_lkw()
+
+    def follow_lkw(self):
+        if self.lkw.rect.centerx < self.rect.centerx:
             self.rect.x -= self.geschwindigkeit
-        else:
+        elif self.lkw.rect.centerx > self.rect.centerx:
             self.rect.x += self.geschwindigkeit
-        if self.lkw.rect.y < self.rect.y:
+        if self.lkw.rect.centery < self.rect.centery:
             self.rect.y -= self.geschwindigkeit
-        else:
+        elif self.lkw.rect.centery > self.rect.centery:
             self.rect.y += self.geschwindigkeit
+
+    def reset_to_helipad(self):
+        self.rect.center = self.helipad.rect.center
+        self.zurücksetzen_erforderlich = False
+
+class Helipad(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = load_image('helipad')
+        self.rect = self.image.get_rect(center=(random.randint(50, screen_width - 150), random.randint(50, screen_height - 50)))
+
+class Hubschrauber(pygame.sprite.Sprite):
+    def __init__(self, lkw, helipad):
+        super().__init__()
+        self.image = load_image('helicopter')
+        self.rect = self.image.get_rect(center=helipad.rect.center)
+        self.geschwindigkeit = 2.5
+        self.lkw = lkw
+        self.helipad = helipad
+        self.zurücksetzen_erforderlich = False
+
+    def update(self):
+        if self.zurücksetzen_erforderlich:
+            self.reset_to_helipad()
+        else:
+            self.follow_lkw()
+
+    def follow_lkw(self):
+        if self.lkw.rect.centerx < self.rect.centerx:
+            self.rect.x -= self.geschwindigkeit
+        elif self.lkw.rect.centerx > self.rect.centerx:
+            self.rect.x += self.geschwindigkeit
+        if self.lkw.rect.centery < self.rect.centery:
+            self.rect.y -= self.geschwindigkeit
+        elif self.lkw.rect.centery > self.rect.centery:
+            self.rect.y += self.geschwindigkeit
+
+    def reset_to_helipad(self):
+        self.rect.center = self.helipad.rect.center
+        self.zurücksetzen_erforderlich = False
 
 
 class Erzquelle(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = load_image('erz')  # Bild für die Erzquelle
+        self.image = load_image('erz')
         self.rect = self.image.get_rect(center=(random.randint(100, screen_width - 100), random.randint(50, screen_height - 50)))
         self.erz_menge = 1000
 
     def neu_positionieren(self):
-        self.rect = self.image.get_rect(center=(random.randint(100, screen_width - 100), random.randint(50, screen_height - 50)))
+        self.rect.center = (random.randint(100, screen_width - 100), random.randint(50, screen_height - 50))
 
 
 class Abladeplatz(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = load_image('storage')  # Bild für den Abladeplatz
-        self.rect = self.image.get_rect(center=(screen_width - 100, screen_height // 2))
+        self.image = load_image('storage')
+        self.rect = self.image.get_rect(center=(screen_width - 150, screen_height // 2))
         self.erz = 0
         self.kapazität = 1000
 
@@ -119,24 +178,30 @@ class Abladeplatz(pygame.sprite.Sprite):
 class Tankstelle(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = load_image('fuel')  # Bild für die Tankstelle
-        self.rect = self.image.get_rect(center=(random.randint(100, screen_width - 100), random.randint(50, screen_height - 50)))
+        self.image = load_image('fuel')
+        self.rect = self.image.get_rect(center=(random.randint(50, screen_width - 50), random.randint(50, screen_height - 50)))
 
 
 # Spiellogik
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("ErzCollector")
+
     running = True
     spiel_gewonnen = False
 
+    # Erstelle die Spielobjekte (LKW, Erzquelle, Abladeplatz, Tankstelle, Helipad, Hubschrauber)
     lkw = LKW()
     erzquelle = Erzquelle()
     ablageplatz = Abladeplatz()
     tankstelle = Tankstelle()
-    hubschrauber = Hubschrauber(lkw)
+    helipad = Helipad()
+    hubschrauber = Hubschrauber(lkw, helipad)
 
     alle_sprites = pygame.sprite.Group()
     hubschrauber_group = pygame.sprite.Group()
-    alle_sprites.add(lkw, erzquelle, ablageplatz, tankstelle, hubschrauber)
+    alle_sprites.add(lkw, erzquelle, ablageplatz, tankstelle, helipad, hubschrauber)
     hubschrauber_group.add(hubschrauber)
 
     while running:
@@ -144,33 +209,61 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        alle_sprites.update(erzquelle, ablageplatz, tankstelle, hubschrauber_group)
+        # Aktualisiere Spielobjekte
+        lkw.update(erzquelle, ablageplatz, tankstelle, hubschrauber_group)
+        hubschrauber_group.update()
 
+        # Spiellogik für Kollision und Spritverbrauch etc.
         screen.fill(WHITE)
         alle_sprites.draw(screen)
 
+        # Anzeigen von Spielinformationen wie Sprit, Erz im LKW, etc.
+        font = pygame.font.SysFont(None, 20)
         infos = [
             f'Sprit: {int(lkw.sprit)}',
             f'Erz im LKW: {lkw.erz}',
             f'Erz am Abladeplatz: {ablageplatz.erz}/{ablageplatz.kapazität}'
         ]
-        font = pygame.font.SysFont(None, 24)
-        for index, info in enumerate(infos):
-            text_surface = font.render(info, True, BLACK)
-            screen.blit(text_surface, (10, 10 + index * 20))
+
+        text_width = screen_width / len(infos)
+        for index, text in enumerate(infos):
+            text_surface = font.render(text, True, BLACK)
+            x_position = text_width * index + (text_width - text_surface.get_width()) / 2
+            screen.blit(text_surface, (x_position, 10))
 
         pygame.display.flip()
         clock.tick(FPS)
 
-        if ablageplatz.erz >= ablageplatz.kapazität:
-            spiel_gewonnen = True
+        # Überprüfe Spielendebedingungen
+        if lkw.sprit <= 0 or ablageplatz.erz >= 800:
             running = False
+            # Zeigt ein einfaches "Spiel vorbei" Fenster anstelle eines richtigen Popups
+            font = pygame.font.SysFont("arial", 36)
+            end_message = "Spiel vorbei! Neu starten? (Y/N)"
+            text_surface = font.render(end_message, True, RED)
+            text_rect = text_surface.get_rect(center=(screen_width / 2, screen_height / 2))
+            screen.fill(WHITE)
+            screen.blit(text_surface, text_rect)
+            pygame.display.flip()
 
-    end_game(spiel_gewonnen)
+            # Warte auf Benutzerreaktion
+            wait_for_input = True
+            while wait_for_input:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_y:
+                            main()  # Neustart des Spiels
+                            wait_for_input = False
+                        elif event.key == pygame.K_n:
+                            wait_for_input = False
+                            pygame.quit()
+                            return
+                    elif event.type == pygame.QUIT:
+                        wait_for_input = False
+                        pygame.quit()
+                        return
 
-
-def end_game(gewonnen):
-    print("Spiel gewonnen!" if gewonnen else "Spiel beendet.")
+    pygame.quit()
 
 
 if __name__ == '__main__':
